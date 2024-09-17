d97b8027d1b41fbc9014f283224a09c79e87827b
bszymala89 <72854357+bszymala89@users.noreply.github.com>
2024-09-17 19:00:30 +0200

    Added method in MainController

--- src/main/java/com/example/quiz_game/controllers/MainController.java
+++ src/main/java/com/example/quiz_game/controllers/MainController.java
@@ -1,8 +1,14 @@
 package com.example.quiz_game.controllers;
 
+import com.example.quiz_game.MainApplication;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainController {
     @FXML
@@ -11,5 +17,16 @@ public class MainController {
     @FXML
     protected void onExitBtnClick() {
         Platform.exit();
+    }
+
+
+    @FXML
+    protected void onSettingsBtnClick() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("settings-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 340, 335);
+        Stage settingsStage = new Stage();
+        settingsStage.setTitle("Settings");
+        settingsStage.setScene(scene);
+        settingsStage.show();
     }
 }
