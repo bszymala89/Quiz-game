55a81dcc3fd9c13e3247ab590ac84fcce200e0ad
bszymala89 <72854357+bszymala89@users.noreply.github.com>
2024-09-17 18:59:43 +0200

    Added SettingsController and settings-view

--- src/main/java/com/example/quiz_game/controllers/SettingsController.java
+++ src/main/java/com/example/quiz_game/controllers/SettingsController.java
@@ -1,7 +1,16 @@
 package com.example.quiz_game.controllers;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class SettingsController {
+    @FXML
+    protected void onExitBtnClick() {
+        Platform.exit();
+    }
+    @FXML
+    protected void onCheckboxClick() {
 
-
-
+    }
 }
