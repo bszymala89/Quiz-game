c3b79d547be4e6045cfd1202bf3b5b0791b0fdfc
bszymala89 <72854357+bszymala89@users.noreply.github.com>
2024-09-26 17:58:11 +0200

    Added necessary components for changing screen resolution

--- src/main/java/com/example/quiz_game/controllers/SettingsController.java
+++ src/main/java/com/example/quiz_game/controllers/SettingsController.java
@@ -3,14 +3,44 @@ package com.example.quiz_game.controllers;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.stage.Stage;
 
 public class SettingsController {
+
+    private Stage stage;
+
+    @FXML
+    private ComboBox<String> resolutionComboBox;
+
+    @FXML
+    public void initialize() {
+        resolutionComboBox.getItems().addAll("640x480", "960×540", "1280×720", "1920x1080", "2048×1080", "2560×1440", "3840×2160" );
+    }
+
     @FXML
     protected void onExitBtnClick() {
-        Platform.exit();
+        stage.close();
     }
-    @FXML
-    protected void onCheckboxClick() {
 
+    @FXML
+    protected void onMaximizescreenCheckboxClick() {
+        stage.setMaximized(true);
     }
+
+    @FXML
+    protected void onApplyBtnClick() {
+        //to do add split to string and string to values for resolution
+        resolutionComboBox.getValue();
+    }
+
+    /*@FXML
+    protected void onResolutionComboBoxClick() {
+
+    }*/
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
 }
