779c3a65bd39a898070c5fbff7703d4dd8f10c82
bszymala89 <72854357+bszymala89@users.noreply.github.com>
2024-10-15 19:29:03 +0200

    Added necessary components for the creator

---
+++ src/main/java/com/example/quiz_game/controllers/CreatorController.java
@@ -0,0 +1,21 @@
+package com.example.quiz_game.controllers;
+
+import com.example.quiz_game.MainApplication;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class CreatorController {
+    @FXML
+    protected void onCreateQuizBtnClick() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("create-quiz-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 680, 670);
+        Stage createQuizStage = new Stage();
+        createQuizStage.setTitle("Create Quiz");
+        createQuizStage.setScene(scene);
+        createQuizStage.show();
+    }
+}
