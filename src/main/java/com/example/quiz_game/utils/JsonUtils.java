9ac2ecc963553d46fa09d0fae740a68d924100fd
bszymala89 <72854357+bszymala89@users.noreply.github.com>
2024-10-22 19:27:06 +0200

    Added JsonUtils

---
+++ src/main/java/com/example/quiz_game/utils/JsonUtils.java
@@ -0,0 +1,39 @@
+package com.example.quiz_game.utils;
+
+import com.example.quiz_game.Question;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+
+import java.io.*;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JsonUtils {
+    private static final Gson gson = new Gson();
+
+
+    public static List<Question> readFromJson(String path) throws IOException {
+        try(FileReader reader = new FileReader(path)) {
+            Type questionListType = new TypeToken<List<Question>>() {}.getType();
+            List<Question> questionList = gson.fromJson(reader, questionListType);
+            reader.close();
+            if (questionList == null) {
+                return new ArrayList<>();
+            }
+            return questionList;
+        }
+    }
+    public static void saveToJson(String path, Question question) throws IOException {
+        List<Question> questionList = readFromJson(path);
+        try(Writer writer = Files.newBufferedWriter(Paths.get(path));) {
+            questionList.add(question);
+            gson.toJson(questionList, writer);
+            writer.flush();
+            writer.close();
+        }
+    }
+}
