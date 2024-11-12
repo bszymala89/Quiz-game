80c1c4bb23a3cef44def6d8bbf9d99d2f340a3fb
bszymala89 <72854357+bszymala89@users.noreply.github.com>
2024-11-12 19:38:34 +0100

    opened quiz_game package to gson

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,7 @@ module com.example.quiz_game {
     requires javafx.fxml;
     requires com.google.gson;
 
-
-    opens com.example.quiz_game to javafx.fxml;
+    opens com.example.quiz_game to com.google.gson;
     exports com.example.quiz_game;
     exports com.example.quiz_game.controllers;
     opens com.example.quiz_game.controllers to javafx.fxml;
